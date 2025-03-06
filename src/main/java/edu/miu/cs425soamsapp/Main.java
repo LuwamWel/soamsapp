9c2f27ffa16f1a74a36821ae120e998ab97d62f6
LuwamWel <luwamgwm@gmail.com>
2025-03-06 11:56:20 -0600

    added new patient with id 104

--- src/main/java/edu/miu/cs425soamsapp/Main.java
+++ src/main/java/edu/miu/cs425soamsapp/Main.java
@@ -10,8 +10,8 @@ public static void main(String[] args) {
     var publishers = List.of(
             new Patient(100,"jonh", "doe", "john.com", "123-456-9876"),
             new Patient(102,"smith", "doe", "smith.com", "123-456-9876"),
-            new Patient(103,"Mike", "Adam", "mike.com", "123-456-9876")
-            //new Patient(100,"jonh", "doe", "john.com", "123-456-9876"),
+            new Patient(103,"Mike", "Adam", "mike.com", "123-456-9876"),
+            new Patient(104,"luwam", "wel", "luwam.com", "123-456-9876")
     );
 
     publishers.forEach(System.out::println);
